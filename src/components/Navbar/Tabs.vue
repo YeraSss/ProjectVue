e7952aae8702e30f41b21f07dbd89586d468d2a2
aituarkab <15desperado15@gmail.com>
2023-08-21 10:30:19 +0600

    add condition to documents-tab

--- src/components/Navbar/Tabs.vue
+++ src/components/Navbar/Tabs.vue
@@ -1,37 +1,42 @@
 <template>
-    <v-tabs>
-      <v-tab @click="selectedTab = 'categories'" :class="{'custom-tab': true, 'reports-tab': true, 'active-tab': selectedTab === 'categories'}">
-        Отчеты
-      </v-tab>
-      <v-tab @click="selectedTab = 'documents'" :class="{'custom-tab': true, 'documents-tab': true, 'active-tab': selectedTab === 'documents'}">
-        Документы
-      </v-tab>
-    </v-tabs>
-  
-    <div  v-if="selectedTab === 'categories'" class="content" :class="{'active-content': selectedTab === 'categories'}">
-      <categories />
-    </div>
-    <div v-if="selectedTab === 'documents'" class="content" :class="{'active-content': selectedTab === 'documents'}">
-      <documents />
-    </div>
-  </template>
+  <v-tabs>
+    <v-tab @click="selectedTab = 'categories'" :class="{'custom-tab': true, 'reports-tab': true, 'active-tab': selectedTab === 'categories'}">
+      Отчеты
+    </v-tab>
+    <v-tab @click="selectedTab = 'documents'" :class="{'custom-tab': true, 'documents-tab': true, 'active-tab': selectedTab === 'documents'}" v-if="docCategoriesLength > 0">
+      Документы
+    </v-tab>
+  </v-tabs>
+
+  <div v-if="selectedTab === 'categories'" class="content" :class="{'active-content': selectedTab === 'categories'}">
+    <categories />
+  </div>
+  <div v-if="selectedTab === 'documents'" class="content" :class="{'active-content': selectedTab === 'documents'}">
+    <documents />
+  </div>
+</template>
   
   <script>
-  import Categories from './Categories.vue';
-  import Documents from './Documents.vue';
-  
-  export default {
-    components: {
-      Categories,
-      Documents,
+import Categories from './Categories.vue';
+import Documents from './Documents.vue';
+
+export default {
+  components: {
+    Categories,
+    Documents,
+  },
+  data() {
+    return {
+      selectedTab: 'categories',
+    };
+  },
+  computed: {
+    docCategoriesLength() {
+      return this.$store.state.docCategories.length;
     },
-    data() {
-      return {
-        selectedTab: 'categories'
-      };
-    }
-  };
-  </script>
+  },
+};
+</script>
   
   <style>
   .custom-tab {
